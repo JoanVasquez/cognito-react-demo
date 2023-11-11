e09c5289891d912b63ec8ac583fe812a310dbd76
Joan Vasquez <yoloprogramo22@gmail.com>
2023-11-11 03:59:41 -0400

    initial commit, practicing congnito in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Amplify } from "aws-amplify";
+import { awsExports } from "./aws-export";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_CLIENT_ID,
+  },
+});
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
+
+  useEffect(() => {
+    fetchJwtToken();
+  }, []);
+
+  const fetchJwtToken = async () => {
+    try {
+      const session = await Auth.currentSession();
+      const token = session.getIdToken().getJwtToken();
+      setJwtToken(token);
+    } catch (error) {
+      console.error("Error fetching JWT token:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator
+      initialState="signIn"
+      components={{
+        SignUp: {
+          FormFields() {
+            return (
+              <>
+                <Authenticator.SignUp.FormFields />
+
+                {/* Custom fields for given_name and family_name */}
+                <div>
+                  <label>First name</label>
+                </div>
+                <input
+                  type="text"
+                  name="given_name"
+                  placeholder="Please enter your first name"
+                />
+                <div>
+                  <label>Last name</label>
+                </div>
+                <input
+                  type="text"
+                  name="family_name"
+                  placeholder="Please enter your last name"
+                />
+                <div>
+                  <label>Email</label>
+                </div>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Please enter a valid email"
+                />
+              </>
+            );
+          },
+        },
+      }}
+      services={{
+        async validateCustomSignUp(formData): Promise<any> {
+          if (!formData.given_name) {
+            return {
+              given_name: "First Name is required",
+            };
+          }
+          if (!formData.family_name) {
+            return {
+              family_name: "Last Name is required",
+            };
+          }
+          if (!formData.email) {
+            return {
+              email: "Email is required",
+            };
+          }
+        },
+      }}
+    >
+      {({ signOut, user }) => (
+        <div>
+          <p>welcome {user?.username}</p>
+          <button onClick={signOut}>sign out</button>
+          <h4>Your JWT Token is: {jwtToken}</h4>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
---
+++ src/aws-export.ts
@@ -0,0 +1,5 @@
+export const awsExports = {
+  REGION: "us-east-1",
+  USER_POOL_ID: "us-east-1_diUSdiHym",
+  USER_POOL_CLIENT_ID: "4debm6g4hk5mfl99dgphaustek",
+};
